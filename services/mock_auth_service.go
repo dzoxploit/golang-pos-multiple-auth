7b160a0a04aad1adad9652dd54fb18c472c8e457
Didin Yahya <didinnuryahya@gmail.com>
2023-08-07 00:32:26 +0700

    add fix feature

--- services/mock_auth_service.go
+++ services/mock_auth_service.go
@@ -4,7 +4,7 @@ import "errors"
 
 type MockAuthService struct{}
 
-func (s *MockAuthService) Login(username, password string) (string, error) {
+func (s *MockAuthService) Login(email, username, password string) (string, error) {
 	// Implement the login functionality for the mock service.
 	// For testing purposes, you can return a fixed access token or error.
 	var ErrInvalidCredentials = errors.New("invalid credentials")
